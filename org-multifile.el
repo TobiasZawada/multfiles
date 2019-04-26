a07cff2117c6ec239390b54572d90c70042f4f92
TobiasZawada <i@tn-home.de>
2019-04-26 14:23:22 +0200

    Create org-multifile.el

---
+++ org-multifile.el
@@ -0,0 +1,66 @@
+;;; org-multifile.el --- Combine multiple org files  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Tobias Zawada
+
+;; Author: Tobias Zawada <naehring@smtp.1und1.de>
+;; Keywords: outlines, files
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(require 'multfiles)
+
+(defun org-multifile (re &optional dir file-name-re)
+  "Collect org-entries matching regular expression RE in one multi-file-buffer."
+  (interactive (list
+		(read-regexp "Regular expression for header line:")
+		(read-directory-name "Directory:")
+		(read-regexp "Regular expression for org-files:" "^20.*\.org$")))
+  (require 'elgrep)
+  (unless file-name-re (setq file-name-re "\\.org$"))
+  (loop for filematch in (elgrep dir file-name-re (concat "^\\*.*" re)
+				 :c-op (lambda (beg end)
+					(cons beg
+					      (save-excursion
+						(save-match-data
+						  (let (at-head)
+						    (while (and (setq at-head (re-search-forward "^* " nil 'noErr))
+								(org-in-block-p '("src"))))
+						    (if at-head (line-end-position 0)
+						      (point))
+						    )))))
+				 :abs t)
+	do
+	(with-current-buffer (find-file (car filematch))
+	  (loop for matchdata in (cdr filematch) do
+		(let* ((context (plist-get (car matchdata) :context))
+		       (b (car context))
+		       (e (cdr context)))
+		  (mf/mirror-region-in-multifile b e (concat "*mf:" re) "* [[file:" "]]"))))))
+
+
+(defun org-multifile-master (buffer-or-file-name)
+  ""
+  (let ((include-re "^[ \t]*#\\+INCLUDE:"))
+    
+    ))
+
+
+(provide 'org-multifile)
+;;; org-multifile.el ends here
